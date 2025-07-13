bedbe30733f29e3bf99def1e477a91611d225de8
GayatriSatram3 <satramgayatri@gmail.com>
2025-07-13 14:38:32 +0530

    Add files via upload

---
+++ color.js
@@ -0,0 +1,22 @@
+let bgContainer = document.getElementById("bgContainer");
+let changeColor = document.getElementById("changeColor");
+function box1(){
+    bgContainer.style.backgroundColor = "#e0e0e0";
+    changeColor.textContent = "#e0e0e0";
+    changeColor.style.color = "#e0e0e0";
+}
+function box2(){
+    bgContainer.style.backgroundColor = "#6fcf97";
+    changeColor.textContent = "#6fcf97";
+    changeColor.style.color = "#6fcf97";
+}
+function box3(){
+    bgContainer.style.backgroundColor = "#56ccf2";
+    changeColor.textContent = "#56ccf2";
+    changeColor.style.color = "#56ccf2";
+}
+function box4(){
+    bgContainer.style.backgroundColor = "#bb6bd9";
+    changeColor.textContent = "#bb6bd9";
+    changeColor.style.color = "#bb6bd9";
+}
